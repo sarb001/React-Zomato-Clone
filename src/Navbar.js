456bfdfbafd8019face7b67df3e62adaa4494682
SARB <mrsinghbusiness05@gmail.com>
2022-10-26 21:33:57 +0530

    Pending

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,14 +3,22 @@ import './Navbar.css';
 
  function Navbar() {
   return (
-    <div className='navbar-main'>
-            <h2>  Isdie  </h2>
-            <ul>
-                <li> <a href = "#" > Investor Relations </a> </li>
-                <li> <a  href = "#"> Add restaurant </a> </li>
-                <li> <a  href = "#"> Log in  </a> </li>
-                <li> <a  href = "#"> Sign up </a> </li>
-            </ul>
+    <div className = 'navbar-bg'>
+        <div className="navbar-content">
+            <div className="navbar-first">
+                    <span> Get the App  </span>
+            </div>
+            <div className="navbar-second">
+                    <ul>
+                        <li> <a href = "#" > Investor Relations </a> </li>
+                        <li> <a  href = "#"> Add restaurant </a> </li>
+                        <li> <a  href = "#"> Log in  </a> </li>
+                        <li> <a  href = "#"> Sign up </a> </li>
+                    </ul>
+            </div>
+                  
+        </div>
+            
     </div>
   )
 }
