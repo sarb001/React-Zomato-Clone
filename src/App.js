76d1ea738246293bd60c58b3a42c5d699a207cf7
SARB <mrsinghbusiness05@gmail.com>
2022-10-26 20:24:46 +0530

    Bg Imported Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 
+import Accordian from './Accordian';
 import Home from './Home';
 
 function App() 
 {
   return (
     <div className="App">
+            {/* <Accordian /> */}
             <Home />
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css';
+
+ function Navbar() {
+  return (
+    <div className='navbar-main'>
+            <h2>  Isdie  </h2>
+            <ul>
+                <li> <a href = "#" > Investor Relations </a> </li>
+                <li> <a  href = "#"> Add restaurant </a> </li>
+                <li> <a  href = "#"> Log in  </a> </li>
+                <li> <a  href = "#"> Sign up </a> </li>
+            </ul>
+    </div>
+  )
+}
+
+export default  Navbar
