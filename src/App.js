a00956ae41fbeeaf26cce36be28f7118f9736080
SARB <mrsinghbusiness05@gmail.com>
2022-10-26 19:57:08 +0530

    Accordian Done:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+import Home from './Home';
+
+function App() 
+{
   return (
     <div className="App">
-        <h2>  Ddlgiboj </h2>
+            <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+
+ function Home() {
+
+    const [show,setshow] = useState(false);
+
+  return (
+    <div className='home-side' style = {{padding:'3%' ,cursor:'pointer'}}>
+        <div className = "accordian" onClick = {() => setshow(!show)}>
+            <span style = {{fontSize:'24px'}}>  Cities we deliver to  </span>
+            <span style = {{fontSize:'25px'}}>  {  show ? "-": "+" }  </span>    
+        </div>
+
+            {show ? <p id = "para-data" style = {{width:'20%',fontSize:'18px'}}>   
+                        India Lorem ipsum dolor sit amet, consectetur 
+                        adipisicing elit. Fugit molestiae eligendi sed 
+                        numquam! Cumque iusto ullam alias, molestiae qui
+                        s aliquam, consectetur ab, corporis iure pariatur
+                        quae earum provident incidunt repellendus expedita
+                        consequuntur quaerat qui. Harum nisi sed
+                        modi totam accusamus sequi accusantium 
+                        labore nulla. Alias?
+                    </p> : null }
+        
+    </div>
+  )
+}
+export default Home
