233c6a9e75da04ec2c384651b098a0f59c6bbba8
SARB <mrsinghbusiness05@gmail.com>
2022-10-27 06:56:44 +0530

    Input-bar started

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Navbar.css';
+import {FcSearch} from 'react-icons/fc';
 
  function Navbar() {
   return (
     <div className = 'navbar-bg'>
         <div className="navbar-content">
-            <div className="navbar-first">
+            <div className = "navbar-first">
                     <span> Get the App  </span>
             </div>
-            <div className="navbar-second">
+            <div className = "navbar-second">
                     <ul>
                         <li> <a href = "#" > Investor Relations </a> </li>
                         <li> <a  href = "#"> Add restaurant </a> </li>
@@ -18,7 +19,18 @@ import './Navbar.css';
             </div>
                   
         </div>
-            
+
+            <div className="image" style = {{color:"whitesmoke"}}>
+                <img src = "/zomato_white_logo-removebg-preview.png" />
+                <span>  Discover the best foods &  drinks  </span>
+            </div>
+
+                <div className="input-bar">
+                <FcSearch />
+                     <input  type = "text"  />
+                </div>
+
+
     </div>
   )
 }
