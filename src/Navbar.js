7820fbda1a215b7dea5b18bc973c4d17b9480946
SARB <mrsinghbusiness05@gmail.com>
2022-10-27 07:39:40 +0530

    inputbar Done

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,34 +4,42 @@ import {FcSearch} from 'react-icons/fc';
 
  function Navbar() {
   return (
-    <div className = 'navbar-bg'>
-        <div className="navbar-content">
-            <div className = "navbar-first">
-                    <span> Get the App  </span>
-            </div>
-            <div className = "navbar-second">
-                    <ul>
-                        <li> <a href = "#" > Investor Relations </a> </li>
-                        <li> <a  href = "#"> Add restaurant </a> </li>
-                        <li> <a  href = "#"> Log in  </a> </li>
-                        <li> <a  href = "#"> Sign up </a> </li>
-                    </ul>
-            </div>
-                  
-        </div>
+            <div className ='container'>
+                <div className = 'navbar-bg'>
+                        <div className="navbar-content">
+                            <div className = "navbar-first">
+                                    <span> Get the App  </span>
+                            </div>
+                            <div className = "navbar-second">
+                                    <ul>
+                                        <li> <a  href = "#"> Investor Relations </a> </li>
+                                        <li> <a  href = "#"> Add restaurant </a> </li>
+                                        <li> <a  href = "#"> Log in  </a> </li>
+                                        <li> <a  href = "#"> Sign up </a> </li>
+                                    </ul>
+                            </div>
+                                
+                        </div>
 
-            <div className="image" style = {{color:"whitesmoke"}}>
-                <img src = "/zomato_white_logo-removebg-preview.png" />
-                <span>  Discover the best foods &  drinks  </span>
-            </div>
+                            <div className = "zomato-info" >
+                                <span>   <img src = "/zomato_white_logo-removebg-preview.png"  alt = "zomato-logo"  style = {{width:'20%'}} />  </span>  
+                                <span style = {{fontSize:'45px'}}>  Discover the best foods &  drinks in Phillaur   </span>
+                            </div>
 
-                <div className="input-bar">
-                <FcSearch />
-                     <input  type = "text"  />
+                                <div className="input-cover">
+                                    <div className = "input-bar">
+                                        <span id = "search-icon" >  <FcSearch />  </span>
+                                        <span> <input  type = "text" id = "input-main"  />    </span>
+                                        
+                                    </div>
+                                </div>
                 </div>
 
+                 <div className="section-second">
+                     <h2> Seodn   </h2>
+                 </div>
 
-    </div>
+            </div>
   )
 }
 
